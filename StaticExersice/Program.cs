a23fc5801249f59325ee92877145a363234653ee
Henry Lee <hlee09151@gmail.com>
2022-05-04 14:52:26 -0500

    init

---
+++ StaticExersice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StaticExersice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var temp1 = TempConverter.FahrenheitToCelsius(75.00);
+            Console.WriteLine($"This tempurture in Fahrenheit {75.00} is this in Celcius {temp1}");
+            var temp2 = TempConverter.CelsiusToFarenheit(21.81);
+            Console.WriteLine($"This tempurture in Celcius {21.81} is this in Fehrenheit {temp2}");
+     
+        }
+
+    }
+}
---
+++ StaticExersice/TempConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExersice
+{
+    //Create a new console app called StaticExercise.
+//Now create a static class called TempConverter.The class should have at least 2 methods,
+//one called FahrenheitToCelsius that will require a double as a parameter and return a double,
+//the other CelsiusToFahrenheit which will also require a double as a parameter and return a double.
+//Fill out these methods and call them in your Program.cs file.Make sure they’re accurate!
+//(farTemperature - 32) / 1.8
+
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double farTC)
+        {
+           double answer = (farTC - 32) / 1.8;
+            return answer;
+
+        }
+        public static double CelsiusToFarenheit(double celTF)
+        {
+            double answer1 = (celTF * 1.8) + 32;
+            return answer1;
+        }
+
+    }
+
+}
